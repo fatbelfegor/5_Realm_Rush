e0e1fb0ffb4fb1760b9db9d3003c2d136d74c206
Klim Agarkov <jewelgatorstv@gmail.com>
2018-09-20 22:32:56 +0300

    Add block dictionary

--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -13,11 +13,16 @@ public class Waypoint : MonoBehaviour {
         return gridSize;
     }
 
-    public Vector2 GetGridPos()
+    public Vector2Int GetGridPos()
     {
         return new Vector2Int(
             Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
             Mathf.RoundToInt(transform.position.z / gridSize) * gridSize
         );
+    }
+
+    public void SetTopColor()
+    {
+
     }
 }
