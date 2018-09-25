5862f78e4dfb8cd53b93b94c05fec0bef26257c2
Klim Agarkov <jewelgatorstv@gmail.com>
2018-09-25 22:34:23 +0300

    Add neighbours coloring

--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -8,13 +8,33 @@ public class Pathfinder : MonoBehaviour {
     [SerializeField] Waypoint startWaypoint, endWaypoint;
 
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Vector2Int[] directions = {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
 
 	// Use this for initialization
-	void Start () {
+	void Start() {
         LoadBlocks();
         colorStartAndEnd();
+        ExploreNeighbours();
 		
 	}
+
+    private void ExploreNeighbours()
+    {
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int explorationCoordinates = startWaypoint.GetGridPos() + direction;
+            try
+            {
+                grid[explorationCoordinates].SetTopColor(Color.blue);
+            }
+            catch { }
+        }
+    }
 
     private void colorStartAndEnd()
     {
