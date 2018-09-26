a8711abb5ff022dda3fc1221b2e3ead3da8e007c
Klim Agarkov <jewelgatorstv@gmail.com>
2018-09-27 01:00:27 +0300

    Add the first implementation of pathfinding

--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -3,11 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pathfinder : MonoBehaviour {
+public class Pathfinder : MonoBehaviour
+{
 
     [SerializeField] Waypoint startWaypoint, endWaypoint;
 
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Queue<Waypoint> queue = new Queue<Waypoint>();
+    [SerializeField] bool isRunning = true; // todo make private
+
     Vector2Int[] directions = {
         Vector2Int.up,
         Vector2Int.right,
@@ -15,28 +19,75 @@ public class Pathfinder : MonoBehaviour {
         Vector2Int.left
     };
 
-	// Use this for initialization
-	void Start() {
-        LoadBlocks();
-        colorStartAndEnd();
-        ExploreNeighbours();
-		
-	}
-
-    private void ExploreNeighbours()
+    // Use this for initialization
+    void Start()
     {
-        foreach (Vector2Int direction in directions)
+        LoadBlocks();
+        ColorStartAndEnd();
+        Pathfind();
+        //ExploreNeighbours();
+    }
+
+    private void Pathfind()
+    {
+        queue.Enqueue(startWaypoint);
+
+        while (queue.Count > 0 && isRunning)
         {
-            Vector2Int explorationCoordinates = startWaypoint.GetGridPos() + direction;
-            try
-            {
-                grid[explorationCoordinates].SetTopColor(Color.blue);
-            }
-            catch { }
+            var searchCenter = queue.Dequeue();
+            print("Searching from: " + searchCenter); // todo remove log
+            HaltIfEndFound(searchCenter);
+            ExploreNeighbours(searchCenter);
+            searchCenter.isExplored = true;
+        }
+
+        // todo work-out path!
+        print("Finished pathfinding?");
+    }
+
+    private void HaltIfEndFound(Waypoint searchCenter)
+    {
+        if (searchCenter == endWaypoint)
+        {
+            print("Searching from end node, therefore stopping"); // todo remove log
+            isRunning = false;
         }
     }
 
-    private void colorStartAndEnd()
+    private void ExploreNeighbours(Waypoint from)
+    {
+        if (!isRunning) { return; }
+
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int neighbourCoordinates = from.GetGridPos() + direction;
+            try
+            {
+                QueueNewNeighbours(neighbourCoordinates);
+            }
+            catch
+            {
+                // do nothing
+            }
+        }
+    }
+
+    private void QueueNewNeighbours(Vector2Int neighbourCoordinates)
+    {
+        Waypoint neighbour = grid[neighbourCoordinates];
+        if (neighbour.isExplored)
+        {
+            // do nothing
+        }
+        else
+        {
+            neighbour.SetTopColor(Color.blue); // todo move later
+            queue.Enqueue(neighbour);
+            print("Queueing " + neighbour);
+        }
+    }
+
+    private void ColorStartAndEnd()
     {
         startWaypoint.SetTopColor(Color.green);
         endWaypoint.SetTopColor(Color.red);
@@ -50,12 +101,14 @@ public class Pathfinder : MonoBehaviour {
             var gridPos = waypoint.GetGridPos();
             if (grid.ContainsKey(gridPos))
             {
-                Debug.LogWarning("Skipping overlapping  block " + waypoint);
+                Debug.LogWarning("Skipping overlapping block " + waypoint);
             }
             else
             {
                 grid.Add(gridPos, waypoint);
             }
         }
+
+
     }
 }
